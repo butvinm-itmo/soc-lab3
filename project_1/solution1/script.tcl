1ceb7c26fc31f5cce74d44bd6e971d00947832e1
Mihail Butvin <butvin.mihail@yandex.ru>
2025-11-27 04:17:48 +0300

    Add other neccessary files

---
+++ project_1/solution1/script.tcl
@@ -0,0 +1,19 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project project_1
+set_top fir
+add_files fir.c
+add_files -tb out.gold.dat -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb fir_test.c -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+open_solution "solution1"
+set_part {xc7a100t-csg324-1} -tool vivado
+create_clock -period 10 -name default
+config_export -format ip_catalog -rtl verilog
+#source "./project_1/solution1/directives.tcl"
+csim_design
+csynth_design
+cosim_design
+export_design -rtl verilog -format ip_catalog
